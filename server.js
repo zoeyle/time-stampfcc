5b8ee6bb42636c37a8847fa1ffaa6314820f833c
Glitch (time-stampfcc) <none>
2018-01-04 15:21:39 +0000

    👴⚓️ Checkpoint server.js:565149

--- server.js
+++ server.js
@@ -3,37 +3,42 @@
 
 // init project
 var express = require('express');
+var url = require('url');
 var app = express();
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+// // http://expressjs.com/en/starter/basic-routing.html
+// app.get("/", function (request, response) {
+//   response.sendFile(__dirname + '/views/index.html');
+// });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
+// app.get("/dreams", function (request, response) {
+//   response.send(dreams);
+// });
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
+// // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+// app.post("/dreams", function (request, response) {
+//   dreams.push(request.query.dream);
+//   response.sendStatus(200);
+// });
 
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+// // Simple in-memory store for now
+// var dreams = [
+//   "Find and count some sheep",
+//   "Climb a really tall mountain",
+//   "Wash the dishes"
+// ];
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+// // listen for requests :)
+// var listener = app.listen(process.env.PORT, function () {
+//   console.log('Your app is listening on port ' + listener.address().port);
+// });
+
+function parseUnix(time){
+  
+};
